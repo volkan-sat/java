78c35c59526341e63e7379c5d2e5d223a5a85152
volkan-sat <s.volkansatiroglu@gmail.com>
2024-11-29 15:31:40 +0300

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,12 @@
+const vol = document.getElementById("btn");
+
+vol.onclick = function() {
+    vol.innerHTML = "GİRİŞ BAŞARILI"; 
+};
+
+const sar= document.getElementById("rtx");
+
+sar.onclick = function() {
+    sar.innerHTML = "GERİ DÖN"; 
+};
+
